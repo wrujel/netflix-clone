aa1e12981198c67f7e565cf38c9b5b4db4fab93c
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2023-05-09 13:38:22 -0500

    feat: add navbar menu

---
+++ components/Navbar.tsx
@@ -0,0 +1,121 @@
+import { useCallback, useEffect, useState } from "react";
+import Logo from "./Logo";
+import MobileMenu from "./MobileMenu";
+import NavbarItem from "./NavbarItem";
+import { BsBell, BsChevronDown, BsSearch } from "react-icons/bs";
+import Avatar from "./Avatar";
+import AccountMenu from "./AccountMenu";
+
+const TOP_OFFSET = 66;
+
+const Navbar = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const [showAccountMenu, setShowAccountMenu] = useState(false);
+  const [showBackground, setBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= TOP_OFFSET) {
+        setBackground(true);
+      } else {
+        setBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const toggleMobileMenu = useCallback(() => {
+    setShowMobileMenu((current) => !current);
+  }, []);
+
+  const toggleAccountMenu = useCallback(() => {
+    setShowAccountMenu((current) => !current);
+  }, []);
+
+  return (
+    <nav className="w-full fixed z-40">
+      <div
+        className={`
+          px-4
+          md:px-16
+          py-6
+          flex
+          flex-row
+          items-center
+          transition
+          duration-500
+          ease-in-out
+          bg-zinc-900
+          bg-opacity-90
+          ${showBackground ? "bg-zinc-900 bg-opacity-90" : ""}
+        `}
+      >
+        <Logo className="h-4 lg:h-7 w-auto" />
+        <div
+          className="
+            flex-row
+            ml-8
+            gap-7
+            hidden
+            lg:flex
+          "
+        >
+          <NavbarItem label="Home" />
+          <NavbarItem label="Series" />
+          <NavbarItem label="Films" />
+          <NavbarItem label="New & Popular" />
+          <NavbarItem label="My List" />
+          <NavbarItem label="Browse by languages" />
+        </div>
+        <div
+          onClick={toggleMobileMenu}
+          className="
+            lg:hidden
+            flex
+            flex-row
+            items-center
+            gap-2
+            ml-8
+            cursor-pointer
+            relative
+          "
+        >
+          <p className="text-white text-sm">Browse</p>
+          <BsChevronDown
+            className={`text-white duration-300 transition ${
+              showMobileMenu ? "rotate-180" : "rotate-0"
+            }`}
+          />
+          <MobileMenu visible={showMobileMenu} />
+        </div>
+        <div className="flex flex-row ml-auto gap-7 items-center">
+          <div className="text-gray-200 hover-text-gray-300 cursor-pointer transition">
+            <BsSearch />
+          </div>
+          <div className="text-gray-200 hover-text-gray-300 cursor-pointer transition">
+            <BsBell />
+          </div>
+          <div
+            onClick={toggleAccountMenu}
+            className="flex flex-row items-center gap-2 cursor-pointer relative"
+          >
+            <div className="w-6 h-6 lg:w-10 lg:h-10 rounded-md overflow-hidden">
+              <Avatar />
+            </div>
+            <BsChevronDown
+              className={`text-white duration-300 transition ${
+                showAccountMenu ? "rotate-180" : "rotate-0"
+              }`}
+            />
+            <AccountMenu visible={showAccountMenu} />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage, NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
 import useCurrentUser from "../hooks/useCurrentUser";
+import Navbar from "../components/Navbar";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,12 +25,7 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix</h1>
-      <p className="text-white">Logged in as : {user?.name}</p>
-      <p className="text-white">Logged in as : {user?.email}</p>
-      <button onClick={() => signOut()} className="h-10 w-full bg-white">
-        Log out
-      </button>
+      <Navbar />
     </>
   );
 };
